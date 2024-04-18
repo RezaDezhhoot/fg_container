671125cb9fedf784fb40901188bfcc8594a19a3c
Reza Dezhhoot <rdezhhoot@gmail.com>
2024-04-18 04:12:14 +0330

    update send sms

--- app/Http/Controllers/Sends/SendMessages.php
+++ app/Http/Controllers/Sends/SendMessages.php
@@ -6,32 +6,33 @@ namespace App\Http\Controllers\Sends;
 
 use App\Models\Notification;
 use GuzzleHttp\Client;
+use Illuminate\Support\Arr;
 
 class SendMessages
 {
     private $baseUri = 'https://ws.sms.ir/api/';
     private $apiKey = 'V-qyDXKy60ZVeni8h-HAl6Qtz2vXP4Keenc0EN6k3LQ=';
     private $username = '09931788937';
-    private $password = 'faraz0671834685';
+    private $password = 'a1rp1l5o#{rB';
     private $lineNumber = '3000505';
 
 
     public function sendCode($code , $phone)
     {
-      
-
         $client = new Client();
-        $query = ['apikey' => $this->apiKey,
-            'pid' => 'xwxal1leox0uq1a',
-            'fnum' => $this->lineNumber,
-            'tnum' => $phone,
-            'p1' => 'verification-code',
-            'v1' => $code];
 
-        $result = $client->get('http://ippanel.com:8080/',
-            [
-                'query' => $query,
-            ]);
+        $query = Arr::query([
+            'username' => $this->username,
+            'password' => $this->password,
+            'from' => $this->lineNumber,
+            'to' => $phone,
+            'pattern_code' => "xwxal1leox0uq1a",
+            'input_data' => json_encode([
+                "verification-code" => $code
+            ]),
+        ]);
+
+        $result = $client->post("https://ippanel.com/patterns/pattern"."?$query");
         return json_decode($result->getBody(), true);
     }
 }
